379fbdbc5b3d946f91c61973608356367b8e4ebe
Huy Ky <huyky26@gmail.com>
2025-10-23 18:48:15 -0700

    t6

--- api/index.js
+++
@@ -1,2 +0,0 @@
-// Compatibility wrapper for root-level API routing.
-module.exports = require('../server/api/index.js');
--- api/socketio.js
+++
@@ -1,2 +0,0 @@
-// Compatibility wrapper for root-level Socket.IO function.
-module.exports = require('../server/api/socketio.js');
